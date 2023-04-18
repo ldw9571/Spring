0692d5d1b76e35a259cd67384e986ddeb4fe7a4f
Soon9 (SoonGu Hong) <naong526@naver.com>
2023-04-18 14:36:01 +0900

    chap01 - solid

--- src/main/java/com/spring/core/CoreApplication.java
+++ src/main/java/com/spring/core/CoreApplication.java
@@ -9,7 +9,7 @@ public class CoreApplication {
 	public static void main(String[] args) {
 //		SpringApplication.run(CoreApplication.class, args);
 
-		System.out.println("hello world");
+		System.out.println("안녕하세요");
 
 	}
 
